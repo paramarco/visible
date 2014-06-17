1ab6135eaede5a2816f451542bbe3628df21d3b0
Marco Vereda Manchego <paramarco@gmail.com>
2014-06-17 15:47:14 +0200

    embellecimiento del Interfaz

--- platforms/android/assets/www/js/interface2AllJoyn.js
+++ platforms/android/assets/www/js/interface2AllJoyn.js
@@ -1,12 +1,28 @@
-window.echo = function(str) {
-			    	cordova.exec(	function(varrabless) {
-										alert('funcionan  .' + varrabless);
-			    					}, 
-			    					function(err) {
-										callback('no  funciona.');
-			    					}, 
-			    					"interface2AllJoyn", 
-			    					"echo",
-			    					[str]
-			    				);
-				};
+/**
+ * @update 20140616 plugin declaration
+ * @author Marco
+ *
+ */
+				
+//CLASS DECLARATION interface2AllJoyn 
+ function interface2AllJoyn ()  {  
+
+//CLASS ATTRIBUTES
+	
+//CLASS CONSTRUCTOR
+
+	
+//CLASS METHODS
+	this.makeCall = function (str) {	
+						cordova.exec(	function(varrabless) {
+											alert('funcionan  :' + varrabless);
+										}, 
+										function(err) {
+											callback('no  funciona.');
+										}, 
+										"interface2AllJoyn", 
+										"echo",
+										[str]
+									);
+					}//END makeCall
+}// END class
